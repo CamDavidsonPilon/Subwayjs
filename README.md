957faf259969aff0faab2a1a950e430ab256cf16
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2012-08-02 12:12:49 -0400

    JS changes to the library

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Subwayjs is a javascript module for creating an interactive subway system for you website. See www.camdp.com for an example of one. 
+Subwayjs is a javascript module for creating an interactive subway system for you website. See www.camdp.com and www.camdp.com/ttc for an examples.
 The module relies on Paper.js (http://paperjs.org/download/). 
 
 To run, you'll need something that looks like this in your header:
@@ -7,6 +7,6 @@ To run, you'll need something that looks like this in your header:
         <script type="text/paperscript" src="/scripts/subway.js" canvas="subway"></script>
         
 Note that there is only one file that can contain BOTH the classes (present aready in subway.js) and the initialization of the stations 
-(see initialization_example.js). The seperation above was purely for asthetics. Please email me at cam.davidson.pilon@gmail.com it you 
+(see initialization_example.js). The seperation above was purely for asthetics. See ttc.js for a full, working example. Please email me at cam.davidson.pilon@gmail.com it you 
 have any comments, questions or suggestions.
 
--- subway.js
+++ subway.js
@@ -1,50 +1,55 @@
+
 //subway.js
 // Author: Cameron Davidson-Pilon
 // Date: July, 2012
-// License: MIT License
+// License: MIT
 
 
 // Dependencies: 
 //  Paper.js (http://paperjs.org/)
 
 
-//subway for blogs.
 
 
-// These change the subway chacteristics. Try manipulating them to see what they do.
+
+// These change the subway node design. Try manipulating them to see what they do.
 // Note: all measurments are in pixels.
-var NODE_SIZE = 16; //radius.
-var INNER_NODE_SIZE = 7; 
-var CUR_NODE_SIZE = 9;
-var LINE_WIDTH = 13;
+
+var NODE_SIZE = 16; //radius of the nodes
+var INNER_NODE_SIZE = 7; //radius of the inner circle in the stations
+var CUR_NODE_SIZE = 9; // radious of the inner cicle, if is is currently selected.
+var LINE_WIDTH = 23; //width of the inter-station lines.
 
 // fonts
 var FONT_SIZE = 16;
 var TEXT_FONT = 'Arial';
+var FONT_COLOR = 'white';
+
 var CHARACTER_STYLE = {
-		fillColor: 'black',
+		fillColor: FONT_COLOR,
 		fontSize: FONT_SIZE,
         font: TEXT_FONT,
 		};
         
-// after initilization, this variable will contain all stations. Needed for clicking with mouse. 
-nodes = []; 
+var nodes = []; // after initilization, this variable will contain all stations. Needed for clicking stations with mouse. 
+var shiftDown = 0; //This is used in controlling the location with the keys. It is a boolean representing whether the 
+                   // shift key is currently pressed down. 
 
 function Line( color, name ){
     // the Line object creates a subway line. 
     //   color: must be a paper.js RgbColor or a keyword color, eg: 'black', 'green'.
     //   name: the unique name of the line. 
 
-    this.color = color;
-
-    this.path = new Path();
-    this.path.strokeWidth = LINE_WIDTH;
-    this.path.strokeColor = this.color;
-    this.name = name;
+    this['color'] = color;
+    this['path'] = new Path();
+    this['path'].strokeWidth = LINE_WIDTH;
+    this['path'].strokeColor = this['color'];
+    this['name'] = name;
 	
 }
 	
 	
+		
 function SingleNode( line, position, neighbours, name, url, name_offset, name_angle){
     // the Node object is a non transitionay subway stop
     //      line: the line the node lies on.
@@ -59,33 +64,44 @@ function SingleNode( line, position, neighbours, name, url, name_offset, name_an
     //                  10 pixels below the center of the station.
     //      name_angle: the angle the name to be at. Enter it in counterclockwise radians.
     
-    this.line = line;
-    this.position = position;
-    this.neighbours = neighbours;
-	this.name = name;
-	this.url = url;
+    this['line'] = line;
+    this['position'] = position;
+    this['neighbours'] = neighbours;
+	this['name'] = name;
+	this['url'] = url;
+    this['name_angle'] = name_angle;
+    this['neighbours'] = neighbours;
 	nodes.push(this)
     
     //Methods
-    this.onkeydownNode = onkeydownNode;
-    this.emptyNode = emptyNode;
-    this.fillNode = fillNode;
+    this.onkeydownNode = onkeydownNode; //this controls the movement if keys are pressed.
+    this.fillNode = function(){
+            //this method fires when the user enters the node.
+            this.ic.fillColor = 'black';
+    }; 
+    this.emptyNode = function(){
+            //this method fires when the user enters the node.
+            this.ic.fillColor = 'white';
+    };
 
+    
     // generate the visual shapes.
-    var c = new Path.Circle( this.position, NODE_SIZE);
-    c.fillColor = this.line.color;
-    var ic = new Path.Circle( this.position, INNER_NODE_SIZE);
-    ic.fillColor = 'white';
+    var c = new Path.Circle( this['position'], NODE_SIZE);
+    c.fillColor = this['line']['color'];
+    var ic = new Path.Circle( this['position'], INNER_NODE_SIZE+2);
+    ic.fillColor = 'black';
+    
+    this.ic = new Path.Circle( this['position'], INNER_NODE_SIZE);
+    this.ic.fillColor = 'white';
 	
-	this.text = new PointText( this.position + name_offset );
-	this.text.content = this.name;
-    this.text.characterStyle = CHARACTER_STYLE;
-	// To rotate names, uncomment below. 
-    this.text.rotate( -name_angle, this.position );
+	this['text'] = new PointText( this['position'] + name_offset );
+	this['text'].content = this['name'];
+    this['text'].characterStyle = CHARACTER_STYLE;
+    this['text'].rotate( -this['name_angle'], this['position'] );
 
 	
     // add it to the line.
-    this.line.path.add( this.position);
+    this['line']['path'].add( this['position'] );
     
     }
     
@@ -106,40 +122,35 @@ function TransitionNode( line, line2, angle, position, neighbours, name, url,nam
     //      name_angle: the angle the name to be at. Enter it in counterclockwise radians.
 
     
-	STROKE_WIDTH = 2; //this two control the offset and appearance of the 'underneath node'
-	CIRCLE_DIST = 16;
+	var STROKE_WIDTH = 2; //this two control the offset and appearance of the 'underneath node'
+	var CIRCLE_DIST = 16;
 	
 	
 	//This Node is the class for line transition stations.
-	this.line = line;
-	this.line2 = line2;
-	this.position = position; //This is the position of either of the circles. The angle determines the position of the other one.
-	this.neighbours = neighbours;
-	this.angle = angle; //This is the angle between the two circles.
-	this.positionAlt = this.position + new Point( CIRCLE_DIST*Math.cos( angle), CIRCLE_DIST*Math.sin( angle)  );
-	this.name = name;
-	this.url = url;
+	this['line'] = line;
+	this['line2'] = line2;
+	this['position'] = position; //This is the position of either of the circles. The angle determines the position of the other one.
+	this['neighbours'] = neighbours;
+	this['angle'] = angle; //This is the angle between the two circles.
+	this['positionAlt'] = this['position'] + new Point( CIRCLE_DIST*Math.cos( angle), CIRCLE_DIST*Math.sin( angle)  );
+	this['name'] = name;
+	this['url'] = url;
 	nodes.push(this);
 	
 	//Methods;
 	this.onkeydownNode = onkeydownNode;
 	this.emptyNode = function(){
-			this.innerc.remove()
+			this.innerc.fillColor = 'white';
 	}
-	//this.fillNode = fillNode;
 	this.fillNode = function(){
-		    this.innerc = new Path.Circle( this.position, CUR_NODE_SIZE);
 			this.innerc.fillColor = 'black';
-//            window.location.href=this.url;
 	} 
 	
-	this.text = new PointText( this.position + name_offset );
-
-	this.text.content = this.name;
-
-    this.text.characterStyle = CHARACTER_STYLE;
-    // uncomment below to rotate.
-    this.text.rotate( -name_angle, this.position);
+    // Title to display.
+	this['text'] = new PointText( this['position'] + name_offset );
+	this['text'].content = this['name'];
+    this['text'].characterStyle = CHARACTER_STYLE;
+    this['text'].rotate( -name_angle, this.position);
 	
 	//generate visual shapes
 	var c2 = new Path.Circle( this.positionAlt , NODE_SIZE);
@@ -153,22 +164,22 @@ function TransitionNode( line, line2, angle, position, neighbours, name, url,nam
 	c1.strokeWidth = STROKE_WIDTH;
 	c1.fillColor = 'white';
 	c1.strokeColor = 'black';
-	var c1 = new Path.Circle( this.position, CUR_NODE_SIZE);
-	c1.fillColor = this.line.color;
+	this.innerc = new Path.Circle( this.position, CUR_NODE_SIZE);
+	innerc.fillColor = this.line.color;
 	
 
 	//add it the the lines.
-	this.line.path.add( this.position);
-	this.line2.path.add(this.positionAlt);
+	this['line'].path.add( this['position']);
+	this['line2'].path.add(this['positionAlt']);
 	
 	
 	}
     
 function LegendItem( start, end, subwayLine ){
     
-     this.line = new Line(subwayLine.color, subwayLine.name);
-     this.line.path.add( start, end)
-     title = new PointText( end + new Point( 7,5) );
+     this['line'] = new Line(subwayLine['color'], subwayLine['name']);
+     this['line']['path'].add( start, end)
+     var title = new PointText( end + new Point( 7,5) );
      title.content = subwayLine.name;
      title.characterStyle = {
 		fillColor: 'black',
@@ -180,49 +191,43 @@ function LegendItem( start, end, subwayLine ){
    
  
 function onKeyDown(event){
-    //this function fires when a key is pressed.
-    if (event.key =='w' ){
-        current_position.onkeydownNode('N');}
-    if (event.key =='a' ){
-        current_position.onkeydownNode('W');}
-    if (event.key == 'd'){
-        current_position.onkeydownNode('E');}
-    if (event.key == 's'){
-        current_position.onkeydownNode('S');}
-        
-    if (event.key in oc(['enter', 'space']) ){
-        //will work if you set station urls. See the doc for SingleNode.
-		window.location.href=current_position.url;
-		}
+    //this function fires when a key is pressed. This uses the shift-<direction> movement.
+    var key = event.key;
+    if (shiftDown == 1){
+            if (key =='up' ){
+                current_position.onkeydownNode('N');}
+            else if (key =='left' ){
+                current_position.onkeydownNode('W');}
+            else if (key == 'right'){
+                current_position.onkeydownNode('E');}
+            else if (key == 'down'){
+                current_position.onkeydownNode('S');}
+    }
+    
+    if (key == 'shift'){
+        shiftDown = 1;
+    }
 }
+
+
+function onKeyUp( event ){
+    if (event.key == 'shift'){
+        shiftDown = 0;
+        }
+}
+
  
-function onkeydownNode( keydown ){
-    for ( key in this.neighbours ){
+function onkeydownNode( keydown, node ){
+    for ( key in this['neighbours'] ){
         if (key == keydown){
             current_position.emptyNode();
-            current_position = this.neighbours[key];
+            current_position = this['neighbours'][key];
             current_position.fillNode();
             }
     }
-    return 0;
-
+    return;
 }
 
-function fillNode(){
-    //this method fires when the user enters the node.
-    var c = new Path.Circle( this.position, CUR_NODE_SIZE);
-    c.fillColor = 'black';
-}
-    
-
-
-function emptyNode(){
-    //this function fires when the user leaves the node;
-    var c = new Path.Circle( this.position, NODE_SIZE);
-    c.fillColor = this.line.color;
-    var ic = new Path.Circle( this.position, INNER_NODE_SIZE);
-    ic.fillColor = 'white';
-}  
 
 
 
@@ -250,9 +255,10 @@ function onMouseDown(event){
     // This function fires whenever the mouse is pressed down. It iteratres through the stations and stops if it finds the 
     // cursor is over a station.
 	var N = nodes.length;
+    var point = event.point;
 	for( var i=0; i<N; i++){
 		var node_position = nodes[i].position;
-		if (Path.Circle( node_position, NODE_SIZE ).bounds.contains(event.point) ){
+		if (Path.Circle( node_position, NODE_SIZE ).bounds.contains(point) ){
 			current_position.emptyNode();
 			current_position = nodes[i];
 			current_position.fillNode();
@@ -264,12 +270,14 @@ function onMouseDown(event){
 function onFrame(event){
     //This control the halo. You can choose to turn it off by removing this function, or you can create your own using 
     //this function too.
-	halo.strokeColor = current_position.line.color;
-    halo.scale( Math.pow( 1.04, Math.sin(event.count/6 )));
-    halo.position = current_position.position ;
-
+	halo.strokeColor = current_position['line']['color'];
+    halo.scale( Math.pow( 1.04, Math.sin(event.count/8 )));
+    halo.position = current_position['position'];
+    
+    //test how long it takes to compute sin(x) for different values of x.
 
 }
+
 
 
 
